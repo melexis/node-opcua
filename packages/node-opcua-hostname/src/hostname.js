5093067d19a48618e7c9ec3acbc30ff3573235ac
Peter Tillemans <pti@melexis.com>
2019-03-07 13:55:35 +0100

    use environment calls to guess hostname

--- packages/node-opcua-hostname/src/hostname.js
+++ packages/node-opcua-hostname/src/hostname.js
@@ -1,6 +1,5 @@
 "use strict";
 const os = require("os");
-const dns = require("dns");
 
 const trim = function (str, length) {
     if (!length) {
@@ -11,36 +10,29 @@ const trim = function (str, length) {
 
 let _fully_qualified_domain_name_cache = null;
 
-function get_fully_qualified_domain_name(optional_max_length) {
+function get_fully_qualified_domain_name2(optional_max_length) {
     if (_fully_qualified_domain_name_cache) {
         return trim(_fully_qualified_domain_name_cache, optional_max_length);
     }
     let fqdn;
+    const env = process.env;
     if (process.platform === "win32") {
 
         // http://serverfault.com/a/73643/251863
-        const env = process.env;
         fqdn = env.COMPUTERNAME + ( (env.USERDNSDOMAIN && env.USERDNSDOMAIN.length > 0) ? "." + env.USERDNSDOMAIN : "");
         _fully_qualified_domain_name_cache = fqdn;
 
     } else {
-
+        console.log(env);
         const hostname = os.hostname();
-        dns.lookup(hostname, (err, ip) => {
-            if (err) {
-                _fully_qualified_domain_name_cache = os.hostname();
-            } else {
-                dns.lookupService(ip, 0, (err, hostname) => {
-                    if (err) {
-                        _fully_qualified_domain_name_cache = os.hostname();
-                    } else {
-                        _fully_qualified_domain_name_cache = hostname;
-                    }
-                });
-            }
-        });
+        if (env.DOMAIN) {
+            _fully_qualified_domain_name_cache = hostname + "." + env.DOMAIN;
+        } else {
+            _fully_qualified_domain_name_cache = hostname;
+        }
     }
     return trim(_fully_qualified_domain_name_cache, optional_max_length);
 }
+
 // note : under windows ... echo %COMPUTERNAME%.%USERDNSDOMAIN%
 exports.get_fully_qualified_domain_name = get_fully_qualified_domain_name;
